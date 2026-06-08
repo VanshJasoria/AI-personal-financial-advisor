f590dc808b9c73f9e38b173cf0de5361d9d205f0
VanshJasoria <vanshjasoria7781@gmail.com>
2026-06-08 16:27:46 +0530

    Create app.py
    Add initial Streamlit application

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="AI Personal Financial Advisor")
+
+st.title("AI Personal Financial Advisor")
+
+st.write("Welcome to Version 1 of the AI Financial Advisor.")
